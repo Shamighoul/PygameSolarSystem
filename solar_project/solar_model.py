e4a6336d4c6ca22122ce611e0f844e76a16d79b8
Shamighoul <71729439+Shamighoul@users.noreply.github.com>
2020-11-11 17:03:54 +0300

    Update solar_model.py

--- solar_project/solar_model.py
+++ solar_project/solar_model.py
@@ -7,11 +7,8 @@ gravitational_constant = 6.67408E-11
 
 def calculate_force(body, space_objects):
     """Вычисляет силу, действующую на тело.
-
     Параметры:
-
     **body** — тело, для которого нужно вычислить дейстующую силу.
-
     **space_objects** — список объектов, которые воздействуют на тело.
     """
 
@@ -20,36 +17,31 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        r = max(r, body.R + obj.R) # обработка аномалий при прохождении одного тела сквозь другое
-        body.Fx += -gravitational_constant*obj.m*body.m*(body.x - obj.x)/r**3
-        body.Fy += -gravitational_constant*obj.m*body.m*(body.y - obj.y)/r**3
+        body.Fx += (gravitational_constant*body.m*obj.m/r**2)*((body.x - obj.x)/r)  # FIXME: нужно вывести формулу...
+        body.Fy += gravitational_constant*body.m*obj.m/r**2*((body.y - obj.y)/r)  # FIXME: нужно вывести формулу...
 
 
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
-
     Параметры:
-
     **body** — тело, которое нужно переместить.
     """
-    old = body.x
-    ax = body.Fx/body.m
-    body.x += body.Vx*dt + ax*dt**2/2
-    body.Vx += ax*dt
-    ay = body.Fy/body.m
-    body.y += body.Vy*dt + ay*dt**2/2
-    body.Vy += ay*dt
 
+    ax = body.Fx/body.m
+    body.x += body.Vx*dt + ax/2*dt**2  # FIXME: не понимаю как менять...
+    body.Vx += ax*dt
+    # FIXME: not done recalculation of y coordinate!
+    ay = body.Fy/body.m
+    body.y += body.Vy*dt + ay/2*dt**2  # FIXME: не понимаю как менять...
+    body.Vy += ay*dt
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.
-
     Параметры:
-
     **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
-
     **dt** — шаг по времени
     """
+
     for body in space_objects:
         calculate_force(body, space_objects)
     for body in space_objects:
